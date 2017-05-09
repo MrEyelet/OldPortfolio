14d99e4f7e4f138f5f15ebd553ad07867274287f
MrEyelet <marcin.adam.mroczko@gmail.com>
2017-05-09 21:27:00 +0200

    old portfolio

---
+++ tab.js
@@ -0,0 +1,31 @@
+
+
+var dane = new Array;
+
+function wymienTresc(id, htmlid, hiperlacze)
+{
+    document.getElementById(htmlid).innerHTML = dane[id];
+    hiperlacze.href = '#';
+}
+
+
+
+
+
+
+
+dane[1] = 'Jestem absolwentem Akademii Górniczo-Hutniczej im. Stanisława Staszica w Krakowie. Obecnie pracuję w laboratorium Instytutu Nauk Geologicznych Polskiej Akademii Nauk  jako geochemik. Przyczyną stworzenia tego  port folio jest chęć  przebranżowienia się z obecnego zawodu na programistę front-endowego. Powodem mojej decyzji jest ogromna chęć nauczenia się przyszłościowej, dobrze płatnej profesji,&nbsp;z&nbsp;którą wiąże się możliwość podnoszenia własnych kwalifikacji. Już w czasach szkolnych lubiłem planować i tworzyć  prace na przedmiotach plastycznych, dlatego wierzę w swój potencjał designerski.';
+
+dane[2] = 'Projekty<br /><br />Poniżej zamieszczam linki do moich małych projektów, w których wykorzystałem podstawową wiedzę z niektórych zagadnień front-endowych. Zachęcam do zapoznania się z nimi i do ich oceny.<br /><br /><br /><ul><li><a href="../MojaPraca/index.html" target="_blank">Moja Praca</a></li><li><a href="../ProstaStrona/index.html" target="_blank">Film</a></li></ul>';
+
+dane[3] = 'W skrócie...<br /><br />Do moich umiejętności miękkich mogę zaliczyć: cierpliwość, dokładność, komunikatywność, otwartość na nową wiedzę oraz kreatwyność. Obecnie uczę się języków programowania takich jak: HTML, CSS, Java Script. Ich podstawy wykorzytałem w moim portfolio. W celu zapoznania się z moim szczegółowym CV i doświadczeniem zawodowym kliknij w poniższy niebieski kafelek, który przekieruje Cię do mojego profilu na LinkedIN.';
+
+dane[4] = 'Strona została napisana w oparciu o:<ul><li>youtube’owe tutoriale Pana Marka Zelenta</li><li>Książkę Html&CSS design and build websites; Jon Ducket</li><li>Książkę CSS nieoficjalny podręcznik, edycja 3; Sawyer McFarland</li><li>kursy z www.codacademy.com</li><li>kursy z www.strefakursow.pl</li><li>wyszukiwarkę Google</li></ul>Stronę starałem się zrobić w pełni responsywną oraz użyłem podstaw javascript’u.';
+
+dane[5] = 'I am a graduate of University of Science and Technology in Cracow. Currently I work  as geochemist  in Polish  Academy of Science in geological laboratory. The cause of the creation of this portfolio is strong desire to change current profession to front end developer. The reason of my decision is willingness to learn well paid, forward looking occupation which provides continuous skill improvement. At school during arts lessons I liked to plan and create therefore I believe in my design potential.';
+
+dane[6] = 'Projects<br /><br />Here are links to my small websites. I used my basic front-end knowledge to create them. I encourage to see my websites and review them.<br /><br /><br /><ul><li><a href="../MojaPraca/indexeng.html" target="_blank">My Job</a></li><li><a href="../ProstaStrona/index.html" target="_blank">Movie</a></li></ul>';
+
+dane[7] = 'In a brief...<br /><br />My soft skills are: patience, accuracy, communication, openness to new knowledge, creativity. Currently I learn programming languages including: HTML, CSS, Java Script. I have used some of basics in my portfolio. In order to get acquainted with my detailed curriculum vitae and work experience click the blue tile below. This will redirect you to my LinkedIN profile.';
+
+dane[8] = 'This website was written based on:<ul><li>Mr Marek Zelent youtube tutorials</li><li>The book Html&CSS design and build websites; Jon Ducket</li><li>The book CCSS the missing manual"; Sawyer McFarland</li><li>kursy z www.codacademy.com</li><li>kursy z www.strefakursow.pl</li><li>Google browser</li></ul>I tried make this website fully responsive and I used some basic script of Javascript.';
